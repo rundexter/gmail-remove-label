dea98b15aa987ad331ec0f788ec6d8c2b9916cf9
Mike Friedman <friedo@friedo.com>
2016-02-23 18:03:01 -0500

    sorting list

--- index.js
+++ index.js
@@ -6,8 +6,23 @@ module.exports = {
      * @param {AppData} dexter Container for all data used in this workflow.
      */
     run: function(step, dexter) {
-        var results = { foo: 'bar' };
-        //Call this.complete with the module's output.  If there's an error, call this.fail(message) instead.
-        this.complete(results);
+        var messages = step.input( 'messages' );
+        var keepnum  = step.input( 'keepnum' );
+
+        var sorted = [ ];
+        messages.each( function( msg ) { sorted.push( msg ) } );
+        sorted.sort( function( a, b ) {
+            if ( a.internalDate < b.internalDate ) return -1;
+            if ( a.internalDate > b.internalDate ) return 1;
+            return 0;
+        } );
+
+        this.log( 'after sorting', { 'list': sorted } );
+
+        sorted = sorted.slice( 0, -keepnum );
+
+        this.log( 'after slice', { 'list': sorted } );
+
+        return this.complete();
     }
 };
