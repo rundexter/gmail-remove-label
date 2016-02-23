fb00f6c007236e85d160b29b520f19c1e7d4bc75
Mike Friedman <friedo@friedo.com>
2016-02-23 18:37:56 -0500

    iterating

--- index.js
+++ index.js
@@ -1,3 +1,20 @@
+var _ = require('lodash'),
+    Q = require('q'),
+    google = require('googleapis'),
+    service = google.gmail('v1');
+
+var unlabel_msg = function( app, service, user, msg_id, label ) {
+    app.log( 'constructing promise for msg id ' + msg_id );
+    var deferred = Q.defer();
+    service.users.messages.modify( { 'id': msg_id, 'userId': user, removeLabelIds: [ label ] }, function( err, message ) {
+        if ( err ) { return deferred.reject( err );      }
+        else       { return deferred.resolve( message ); }
+    } );
+
+    return deferred.promise;
+};
+
+
 module.exports = {
     /**
      * The main entry point for the Dexter module
@@ -6,10 +23,25 @@ module.exports = {
      * @param {AppData} dexter Container for all data used in this workflow.
      */
     run: function(step, dexter) {
+        var OAuth2 = google.auth.OAuth2,
+            oauth2Client = new OAuth2(),
+            credentials = dexter.provider('google').credentials();
+        var inputs = util.pickInputs(step, pickInputs),
+            validateErrors = util.checkValidateErrors(inputs, pickInputs);
+
+        if (validateErrors)
+            return this.fail(validateErrors);
+
+        // set credential
+        oauth2Client.setCredentials({
+            access_token: _.get(credentials, 'access_token')
+        });
+        google.options({ auth: oauth2Client });
+
         var messages = step.input( 'messages' ).first();
         var keepnum  = step.input( 'keepnum' ).first();
-
-        this.log( 'keeping ' + keepnum );
+        var user     = step.input( 'userId' ).first();
+        var label    = step.input( 'label' ).first();
 
         messages.sort( function( a, b ) {
             if ( a.internalDate < b.internalDate ) return -1;
@@ -17,12 +49,16 @@ module.exports = {
             return 0;
         } );
 
-        this.log( 'after sorting', { 'list': messages } );
-
         messages = messages.slice( 0, 0 - keepnum );
 
-        this.log( 'after slice', { 'list': messages } );
+        var removes = [ ];
+        var app = this;
+        messages.forEach( function( item ) {
+            removes.push( unlabel_msg( app, service, user, item.id, label ) );
+        })
 
-        return this.complete();
+        Q.all( removes )
+          .then( function( results ) { app.complete( { 'messages': results } ) } )
+          .fail( function( err ) { app.fail( err ) } );
     }
 };
